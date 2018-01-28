7463e0df0233a223808d8c87e542e19cd9974360
SanSanchez <Ses15r@my.fsu.edu>
2018-01-27 20:53:48 -0500

    Initial Commit

---
+++ Main.js
@@ -0,0 +1,17 @@
+/*
+    Purpose:
+        To ease the task of finding and applying to jobs.
+
+    Initial Proposal:
+        Log into the command line what job I'm applying to, the position, contact information, the date, and
+        possibly the time I should call back if I haven't heard back from them.
+        Aside from that, allow for the addition and removal of jobs as necessary as well as the retrieval of
+        inputted jobs.
+ */
+
+function func()
+{
+    console.log("Hello World!");
+}
+
+func();
